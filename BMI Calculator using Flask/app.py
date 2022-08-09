27359d673318ede30cb4024a919133ab1a2fe4e9
Jyoti Prajapati <52569075+jyotiprajapati98@users.noreply.github.com>
2022-08-09 12:29:22 +0530

    Create app.py

---
+++ BMI Calculator using Flask/app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def calculator():
+    return render_template('BMI_cal.html')
+
+@app.route('/result', methods=['GET', 'POST'])
+def bmi_result():
+    bmi=''
+    result = request.form 
+    if request.method=='POST' and 'Weight' in request.form:
+        weight=float(request.form.get('Weight'))
+        height=float(request.form.get('Height'))
+        bmi = calc_bmi(weight, height)
+        return render_template("result_bmi.html",result=result,bmi=bmi)
+        
+def calc_bmi(w,h):
+    return round((w /((h / 100) ** 2)),2)
+if __name__ == '__main__':
+    app.run(debug=True)
